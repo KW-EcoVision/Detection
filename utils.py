b14b11cd167be128ce8d66a51934ecb3e001fa50
sien <sptw88@gmail.com>
2024-09-16 19:38:40 +0900

    data parse

--- utils.py
+++ utils.py
@@ -1,20 +1,25 @@
 from xml.etree import ElementTree as ET
-
+import json
 import cv2
 import numpy as np
 import cv2 as cv
+from tqdm import tqdm
 from torchvision.transforms import transforms
 import os
 from sklearn.model_selection import train_test_split
 import torch
 import config
+import random
+
 '''
 'xml_path' is mapped by each img path
 'img_dir' is not direct_path just dir
 'img_path' is direct img_path
 '''
-classes_num = {'종이류': 0, '플라스틱': 1, '유리': 2, '캔': 3, '고철류': 4, '의류': 5,
-               '전자제품': 6, '스티로폼': 7, '도기류': 8, '비닐류': 9, '가구': 10, '자전거': 11,
+
+img_path_abs = '/media/sien/DATA/DATA/dataset/eco/생활 폐기물 이미지/Training/img'
+classes_num = {'종이류': 0, '플라스틱류': 1, '유리병류': 2, '캔류': 3, '고철류': 4, '의류': 5,
+               '전자제품': 6, '스티로폼류': 7, '도기류': 8, '비닐류': 9, '가구': 10, '자전거': 11,
                '형광등': 12, '페트병류': 13, '나무류': 14}
 classes_num_rev = {
     0: '종이류',
@@ -34,11 +39,12 @@ classes_num_rev = {
     14: '나무류',
 }
 
-
 IMG_SIZE = 224.0
 
 CELL = 32.0
-def create_label_at_one_xml_path(xml_path,img_dir):
+
+
+def create_label_at_one_xml_path(xml_path, img_dir):
     tree = ET.parse(xml_path)
     root = tree.getroot()
     width = float(root.find('size').find('width').text)
@@ -47,8 +53,8 @@ def create_label_at_one_xml_path(xml_path,img_dir):
     img_path = img_dir + '/' + img_name
 
     label = np.zeros(shape=(7, 7, 25), dtype=float)
-    ration_x = IMG_SIZE/width
-    ratio_y =  IMG_SIZE/height
+    ration_x = IMG_SIZE / width
+    ratio_y = IMG_SIZE / height
 
     for obj in root.findall('object'):
         class_name = obj.find('name').text
@@ -91,14 +97,15 @@ def create_label_at_one_xml_path(xml_path,img_dir):
         label[cell_position_x, cell_position_y][class_id] = 1.0
     return img_path, label
 
+
 def decoding_label(label):
     bbox = []
     class_box = []
 
     for i in range(7):
         for j in range(7):
-            cx = (label[i][j][20] * CELL)+(i*CELL)
-            cy = (label[i][j][21] * CELL)+ (j*CELL)
+            cx = (label[i][j][20] * CELL) + (i * CELL)
+            cy = (label[i][j][21] * CELL) + (j * CELL)
             w = label[i][j][22] * IMG_SIZE
             h = label[i][j][23] * IMG_SIZE
 
@@ -125,16 +132,17 @@ def decoding_label(label):
             class_pred = np.max(label[i][j][:20])
             bbox.append([class_pred, conf, xmin, ymin, xmax, ymax])
             class_box.append(label[i][j][:20])
-    return bbox,class_box
+    return bbox, class_box
 
-def draw(bbox_list,img,class_list):
+
+def draw(bbox_list, img, class_list):
     img = img.astype(np.uint8).copy()
     for i in range(len(bbox_list)):
         img = cv.rectangle(
             img=img,
-            pt1=(bbox_list[i][2],bbox_list[i][3]),
-            pt2=(bbox_list[i][4],bbox_list[i][5]),
-            color=(0,225,0),thickness=1)
+            pt1=(bbox_list[i][2], bbox_list[i][3]),
+            pt2=(bbox_list[i][4], bbox_list[i][5]),
+            color=(0, 225, 0), thickness=1)
         max_val = np.max(class_list[i])
         max_index = np.argmax(class_list[i])
         img = cv2.putText(
@@ -153,22 +161,57 @@ def draw(bbox_list,img,class_list):
 
 def get_augmentor():
     return transforms.Compose([
-        transforms.Resize(size=(int(IMG_SIZE),int(IMG_SIZE))),
+        transforms.Resize(size=(int(IMG_SIZE), int(IMG_SIZE))),
         transforms.ToTensor()
     ])
 
-def split_xml_path(xml_dir):
+
+def get_json_data(json_dir='/media/sien/DATA/DATA/dataset/eco/생활 폐기물 이미지/Training/label'):
     path_list = []
-    cnt=0
-    for file_path, _, file_name in os.walk(xml_dir):
+    for file_path, _, file_name in os.walk(json_dir):
         for name in file_name:
-            full_path = file_path+name
+            full_path = os.path.join(file_path, name)
             path_list.append(full_path)
-    dummy_label = np.zeros(shape=(len(path_list),))
-    train_path,test_path,t_l,e_l = train_test_split(path_list,dummy_label,train_size=0.95,test_size=0.05,shuffle=False)
-    train_path,val_path,_,__  = train_test_split(train_path,t_l,train_size=0.9,test_size=0.1,shuffle=False)
-    val_path,sample_path,___,____ = train_test_split(val_path,__,train_size=0.9,test_size=0.1,shuffle=False)
-    return train_path,val_path,test_path,sample_path
+    label = []
+
+    for i in tqdm(path_list):
+        try:
+            with open(i, 'r', encoding='utf-8') as file:
+                raw_json = json.load(file)
+                file_name = raw_json['FILE NAME']
+                file_name_tmp = file_name.rpartition('_')[0]
+                GPS = raw_json['GPS']
+
+                location = raw_json['Bounding'].pop()
+
+                cls = location['CLASS']
+                detail = location['DETAILS']
+                path = os.path.join(img_path_abs, '[T원천]' + cls + '_' + detail + '_' + detail)
+                x1 = location['x1']
+                y1 = location['y1']
+                x2 = location['x2']
+                y2 = location['y2']
+                tmp_path = os.path.join(path, file_name_tmp, file_name)
+                if os.path.exists(tmp_path):
+                    label.append(dict(img_path=tmp_path, bbox=(x1, y1, x2, y2),
+                                      cls=classes_num[cls], gps=GPS))
+        except json.JSONDecodeError as e:
+            print(f'\n[ERROR] JSONDecodeError 발생\n')
+        except Exception as e:
+            print(f'\n[ERROR] 파일 처리 중 오류 발생\n')
+    random.shuffle(label)
+    with open('./output.json', 'w', encoding='utf-8') as file:
+        json.dump(label, file, ensure_ascii=False, indent=4)
+    return label
+
+def load_and_split_json(dir = 'output.json'):
+    with open(dir,'r',encoding = 'utf-8') as file :
+        raw = json.load(file)
+        l = len(raw)
+        train = raw[:int(l*0.95)]
+        test = raw[int(l*0.95):]
+    return train, test
+
 
 def bbox_to_coords(t):
     """Changes format of bounding boxes from [x, y, width, height] to ([x1, y1], [x2, y2])."""
@@ -185,15 +228,16 @@ def bbox_to_coords(t):
 
     return torch.stack((x1, y1), dim=4), torch.stack((x2, y2), dim=4)
 
+
 def get_iou(p, a):
-    p_tl, p_br = bbox_to_coords(p)          # (batch, S, S, B, 2)
+    p_tl, p_br = bbox_to_coords(p)  # (batch, S, S, B, 2)
     a_tl, a_br = bbox_to_coords(a)
 
     # Largest top-left corner and smallest bottom-right corner give the intersection
     coords_join_size = (-1, -1, -1, config.B, config.B, 2)
     tl = torch.max(
-        p_tl.unsqueeze(4).expand(coords_join_size),         # (batch, S, S, B, 1, 2) -> (batch, S, S, B, B, 2)
-        a_tl.unsqueeze(3).expand(coords_join_size)          # (batch, S, S, 1, B, 2) -> (batch, S, S, B, B, 2)
+        p_tl.unsqueeze(4).expand(coords_join_size),  # (batch, S, S, B, 1, 2) -> (batch, S, S, B, B, 2)
+        a_tl.unsqueeze(3).expand(coords_join_size)  # (batch, S, S, 1, B, 2) -> (batch, S, S, B, B, 2)
     )
     br = torch.min(
         p_br.unsqueeze(4).expand(coords_join_size),
@@ -202,13 +246,13 @@ def get_iou(p, a):
 
     intersection_sides = torch.clamp(br - tl, min=0.0)
     intersection = intersection_sides[..., 0] \
-                   * intersection_sides[..., 1]       # (batch, S, S, B, B)
+                   * intersection_sides[..., 1]  # (batch, S, S, B, B)
 
-    p_area = bbox_attr(p, 2) * bbox_attr(p, 3)                  # (batch, S, S, B)
-    p_area = p_area.unsqueeze(4).expand_as(intersection)        # (batch, S, S, B, 1) -> (batch, S, S, B, B)
+    p_area = bbox_attr(p, 2) * bbox_attr(p, 3)  # (batch, S, S, B)
+    p_area = p_area.unsqueeze(4).expand_as(intersection)  # (batch, S, S, B, 1) -> (batch, S, S, B, B)
 
-    a_area = bbox_attr(a, 2) * bbox_attr(a, 3)                  # (batch, S, S, B)
-    a_area = a_area.unsqueeze(3).expand_as(intersection)        # (batch, S, S, 1, B) -> (batch, S, S, B, B)
+    a_area = bbox_attr(a, 2) * bbox_attr(a, 3)  # (batch, S, S, B)
+    a_area = a_area.unsqueeze(3).expand_as(intersection)  # (batch, S, S, 1, B) -> (batch, S, S, B, B)
 
     union = p_area + a_area - intersection
 
@@ -219,11 +263,14 @@ def get_iou(p, a):
 
     return intersection / union
 
+
 '''
 겹치는 부분 / 전체 박스인데! 귀찮으니까 센터끼지 차이로 보자
 sqrt(pred x,y - label x,y)
 20번째 21번째가 center 좌표
 '''
+
+
 def intersection_over_union(boxes_preds, boxes_labels):
     box1_x1 = boxes_preds[..., 0:1] - boxes_preds[..., 2:3] / 2
     box1_y1 = boxes_preds[..., 1:2] - boxes_preds[..., 3:4] / 2
@@ -233,7 +280,6 @@ def intersection_over_union(boxes_preds, boxes_labels):
     box2_y1 = boxes_labels[..., 1:2] - boxes_labels[..., 3:4] / 2
     box2_x2 = boxes_labels[..., 0:1] + boxes_labels[..., 2:3] / 2
     box2_y2 = boxes_labels[..., 1:2] + boxes_labels[..., 3:4] / 2
-
 
     x1 = torch.max(box1_x1, box2_x1)
     y1 = torch.max(box1_y1, box2_y1)
@@ -247,10 +293,11 @@ def intersection_over_union(boxes_preds, boxes_labels):
 
     return intersection / (box1_area + box2_area - intersection + 1e-6)
 
-def bbox_attr(data, i):
 
+def bbox_attr(data, i):
     attr_start = 20 + i
     return data[..., attr_start::5]
+
 
 def non_max_suppression(bboxes, conf_th, iou_threshold, class_list):
     # 박스 필터링 및 클래스 저장
@@ -266,6 +313,14 @@ def non_max_suppression(bboxes, conf_th, iou_threshold, class_list):
         selected_boxes.append(current_box)
         selected_classes.append(current_class)
         filtered_boxes = [(box, cls) for box, cls in filtered_boxes
-                          if intersection_over_union(torch.Tensor(current_box[2:]),torch.Tensor(box[2:])) <= iou_threshold and max(cls) >0.0 and max(cls<1.0)]
+                          if intersection_over_union(torch.Tensor(current_box[2:]),
+                                                     torch.Tensor(box[2:])) <= iou_threshold and max(cls) > 0.0 and max(
+                cls < 1.0)]
 
     return selected_boxes, selected_classes
+
+
+if __name__ == "__main__":
+    print()
+    get_json_data()
+
